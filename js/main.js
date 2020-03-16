99fe6ad8265dc996f5077e22f29c49e28c1b9fdc
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-03-16 17:55:09 +0100

    Aggiunta note al codice

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// 1 definisco con una variabile lo square cliccato
+// 2 richiamo l'url tramite una chiama ajax
+// 3 definisco con una variabile il numero random api
+// 4 definisco le condizioni
+// 5 Se random <=5 lo square diventa giallo con visualizzazione del numeroRandom all'interno della casella
+// 6 Se random >=6 lo square diventa verde con visualizzazione del numeroRandom all'interno della casella
+
 $(document).ready(function(){
     $('.square').click(function(){
         var thisSquare = $(this);
