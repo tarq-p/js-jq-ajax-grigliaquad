1ddde4c6b20cb14baf91470c506a87813f8b684c
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-03-16 17:23:03 +0100

    ok colorazione caselle in base alla condizione stabilita

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
         $.ajax({
             url: 'https://flynn.boolean.careers/exercises/api/random/int',
             method: 'GET',
-            success: function (numero) {
-                var random = numero.response;
+            success: function (numeroRandom) {
+                var random = numeroRandom.response;
                 thisSquare.text(random);
                 if (random <=5) {
                     thisSquare.addClass('yellow');
-                } else if (random >6) {
+                } else if (random >=6) {
                     thisSquare.addClass('green');
                 }
             },
